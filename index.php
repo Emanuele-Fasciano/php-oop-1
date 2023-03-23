ccd459c6466d95f3dc57bd3dbaf5f4326f03ac96
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-03-23 09:39:08 +0100

    end exercise

--- index.php
+++ index.php
@@ -21,26 +21,26 @@ include_once __DIR__ . "/data/db.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <title>Movies</title>
 </head>
+
 
 <body>
     <div class="container">
         <div class="row">
             <?php foreach ($movies_array as $movie) : ?>
-            <div class="col-3 border me-3">
-                <p><strong>Title:</strong> <?= $movie->title ?></p>
-                <p><strong>Year: </strong><?= $movie->year ?></p>
-                <p><strong>Genre</strong>:</p>
-                <?php foreach ($movie->genre as $genre) : ?>
-                <ul>
-                    <li><?= $genre ?></li>
-                </ul>
-                <?php endforeach ?>
-            </div>
+                <div class="col-3 border me-3">
+                    <p><strong>Title:</strong> <?= $movie->title ?></p>
+                    <p><strong>Year: </strong><?= $movie->year ?></p>
+                    <p><strong>Genre</strong>:</p>
+                    <?php foreach ($movie->genre as $genre) : ?>
+                        <ul>
+                            <li><?= $genre ?></li>
+                        </ul>
+                    <?php endforeach ?>
+                </div>
             <?php endforeach ?>
         </div>
     </div>
